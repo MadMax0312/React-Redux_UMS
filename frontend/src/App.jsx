32cd14cbdd1027158d693fef64b6d92366cbd650
MadMax0312 <thahirmuhammedap@gmail.com>
2024-03-06 10:30:28 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Header from './components/Header'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <>
-    <Header />
-    <h1>My App</h1>
-    </>
-  )
-}
+    return (
+        <>
+            <Header />
+            <Container className='my-2'>
+            <Outlet />
+            </Container>
+      </>
+    );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,36 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='/'>Mern App</Navbar.Brand>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                    <Navbar.Collapse id='basic-navbar-nav'/>
-                    <Nav className='ms-auto'>
-                        <Nav.Link href='/login'>
-                            <FaSignInAlt />Sign In 
-                        </Nav.Link>
-                        <Nav.Link href='/login'>
-                            <FaSignOutAlt />Sign Up
-                        </Nav.Link>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>Mern App</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" />
+                    <Nav className="ms-auto">
+                        <LinkContainer to='/login'>
+                            <Nav.Link>
+                                <FaSignInAlt />
+                                Sign In
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/register'>
+                            <Nav.Link>
+                                <FaSignOutAlt />
+                                Sign Up
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
